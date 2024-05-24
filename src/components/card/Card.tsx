a2409b5b7d33d9b81f86abc6bfaa524a825c618e
Edwilson <edwsilva@gmail.com>
2024-05-24 13:31:16 -0300

    Ajuste do Card quando a procuração estiver ativa

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -37,7 +37,7 @@ const Card = ({ children, titular, inscricao, bg, status }: CardProps) => {
           <Button p textColor="var(--secondary)" text="Aceitar" />
         </div>
       ) : (
-        <div className={styles.buttonContainer}>
+        <div className={styles.buttonContainerAtivas}>
            <Button
             p
             backgroundColor="var(--reject)"
