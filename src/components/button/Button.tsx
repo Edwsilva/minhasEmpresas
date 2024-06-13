eee2b017a20fc9f1f58694b480fe7b305ffca584
eduardo.sa <eduardo.sa@rio.rj.gov.br>
2024-06-13 11:17:48 -0300

    Funcoes de validacao  e toast

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,3 @@
-"use client"
 import { ComponentPropsWithoutRef, Dispatch, ReactNode, SetStateAction, useState } from "react";
 import styles from "./button.module.css";
 
--- src/components/empresas/FormEmpresa.tsx
+++ src/components/empresas/FormEmpresa.tsx
@@ -1,19 +1,23 @@
 import { ChangeEvent, useState } from "react";
 import styles from "./empresas.module.css";
 import Button from "../button/Button";
+import { formatCNPJ, validateCNPJ } from "@/utils/validate";
 
 const FormEmpresa = () => {
   const [textField, setTextField] = useState('');
 
   const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setTextField(e.target.value);
+    const formattedValue = formatCNPJ(e.target.value);
+    setTextField(formattedValue);
   }
 
   const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const validated = validateCNPJ(textField);
 
     console.log("Form Submitted");
-    console.log(textField);
+    console.log(`Textfield - ${textField}`);
+    console.log(`validateCNPJ - ${validated}`);
   }
 
   return (
@@ -29,15 +33,13 @@ const FormEmpresa = () => {
             className={styles.input}
             type="text"
             placeholder="00.000.000/0000-00"
-            maxLength={13}
+            maxLength={18}
             value={textField}
             onChange={inputChangeHandler}
           />
         </div>
         <Button
           p
-          backgroundColor="var(--secondary)"
-          border="1px solid var(--secondary)"
           text="Cadastrar"
           props={{ type: "submit" }}
         />
